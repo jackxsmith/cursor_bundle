315af8cc283aceb424ca172dd36657fbf1f227ab
Jack X. Smith <jackxsmith@example.com>
2025-07-25 07:03:34 -0400

    feat: bump to v6.9.161

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,7 +19,7 @@ variable "environment" {
 variable "app_version" {
   description = "Application version"
   type        = string
-  default     = "6.9.160"
+  default     = "6.9.161"
 }
 
 variable "git_commit" {
